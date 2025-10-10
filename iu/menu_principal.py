2bf0404aafc082206cd1e4210f2d3ad111a80414
jnavf17 <jignacionavarretef14@gmail.com>
2025-10-10 18:26:16 -0300

    jnavf17

--- iu/menu_principal.py
+++ iu/menu_principal.py
@@ -1,12 +1,31 @@
-from auxiliares.info_aplicaion import nombre_aplicacion
+from auxiliares.info_app import nombre_aplicacion
 from auxiliares.version import numero_version
+
+from modelos.comuna import Comuna
+from datos.obtener_datos import obtener_lista_datos
 
 
 def menu_principal():
-    print(f'{nombre_aplicacion} v.{numero_version}')
-    print('=======================================')
     print()
-    print('[1] Gestion Empleados')
-    print('[2] Gestion Proyectos')
-    print('[3] Gestion Horario')
+    print(f'{nombre_aplicacion} {numero_version}')
+    print('====================================')
+    print('[1] Gestionar Talleres')
+    print('[2] Gestionar Personal')
+    print('[3] Gestionar Clientes')
+    print('[4] Gestionar Trabajos')
+    print('[5] Gestionar Comunas')
     print('[0] Salir')
+
+
+def seleccionar_opcion():
+    opcion = input('Seleccione su opción [0-5]: ')
+
+    if opcion == '1':
+        pass
+    elif opcion == '2':
+        pass
+    elif opcion == '5':
+        listado_comunas = obtener_lista_datos('comuna')
+        if len(listado_comunas) > 0:
+            for comuna in listado_comunas:
+                print(f'{comuna.id} {comuna.codigo_comuna} {comuna.nombre_comuna}')
